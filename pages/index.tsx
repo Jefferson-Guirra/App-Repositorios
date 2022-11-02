d5375cdd585d4f9d1be5614fff7077342c5a27e0
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-11-02 20:16:05 -0300

    second commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import * as C from '../styles/home'
+
+export default function Home() {
+  return (
+    <div>
+      <C.h1>Testando</C.h1>
+    </div>
+  )
+}
