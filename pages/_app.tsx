f1f8a4071c5da78bc1100e22a8f06d6073bc735d
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-11-10 12:07:17 -0300

    compras em produção

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,15 @@ import '../styles/global.css'
 import { SessionProvider } from 'next-auth/react'
 import { PayPalScriptProvider} from '@paypal/react-paypal-js'
 
+
+//liveId=AQ4-Jzoq6WQk60K_pvp_krV9qqnTFVtWppdFiwEa3tEOu01bZIXh4Ccd71OHr9GmU3wHHBvM9aAKKCNr
+
+//clientId=AcAcqRy0YXQo8w3nLKJBrwz7QUa14gmObIvRDEWI3Rhu_9Ua-8OSzn4tyHxBsv85X-i78aRBCoE9S7Gu
 const initialOptions = {
   'client-id':
-    'AcAcqRy0YXQo8w3nLKJBrwz7QUa14gmObIvRDEWI3Rhu_9Ua-8OSzn4tyHxBsv85X-i78aRBCoE9S7Gu',
+    'AQ4-Jzoq6WQk60K_pvp_krV9qqnTFVtWppdFiwEa3tEOu01bZIXh4Ccd71OHr9GmU3wHHBvM9aAKKCNr',
   currency: 'BRL',
-  intent: 'capture',
+  intent: 'capture'
 }
 
 
